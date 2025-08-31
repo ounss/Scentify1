08cced446fad716700a326dbecd51a251092734b
Ouns <119680950+ounss@users.noreply.github.com>
2025-08-31 22:07:39 +0200

    backend port

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuration API
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5001/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
