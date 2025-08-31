a888091211c918c6b4c7d56c9a65ade14dae6507
Ouns <119680950+ounss@users.noreply.github.com>
2025-08-31 22:22:10 +0200

    add back

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,6 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Serveur sur port ${PORT}`);
 });
-
 // ✅ CORS CONFIGURATION POUR PRODUCTION
 const corsOptions = {
   origin: function (origin, callback) {
